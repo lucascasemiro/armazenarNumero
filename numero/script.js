e80fb4bfa77e8954c44650611e9f6f0e7a231f98
Lucas Casemiro <141570809+lucascasemiro@users.noreply.github.com>
2024-09-12 11:53:27 -0300

    Add files via upload

---
+++ numero/script.js
@@ -0,0 +1,6 @@
+
+function submitNumber() {
+    number = document.getElementById Number(("number")).value
+    localStorage.setItem("Número", number);
+    console.log("Número registrado com sucesso!")
+}
